7c10aabf523835566eda898ec58ce9fd9b10a44e
Prananda Nur W <widya.nando.wn@gmail.com>
2022-06-06 20:57:45 +0700

    Cleaned the code

--- run_video_app.py
+++ run_video_app.py
@@ -4,73 +4,59 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
 import numpy as np
-import imutils
-import time
 import cv2
-import os
+
 from smbus2 import SMBus
 from mlx90614 import MLX90614
 import RPi.GPIO as GPIO
 import pigpio
-from time import sleep
-from datetime import datetime
-import notify2
-import subprocess
 
 from urllib.request import urlopen
 from azure.storage.blob import BlobServiceClient
 
+import os
+from time import sleep
+from datetime import datetime
+import logging
+
+#azure upload
 AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=raspifpiot;AccountKey=y2Q0OLtu4ycOleEqJ/B5rlMJ3jf3OBwyi85krVwwltyGmpiiYiv4q7oD9kMXWAJm8jdj2/vvMS8j+AStZNgvPw==;EndpointSuffix=core.windows.net"
 CONTAINER_NAME = "captured"
-#import _thread
-#import threading
 
 #thingspeak upload
-baseURL = "https://api.thingspeak.com/update?api_key=DULB71QDT5HZMJIS"
+BASE_URL = "https://api.thingspeak.com/update?api_key=DULB71QDT5HZMJIS"
 
-#initialize temperature sensor bus and gpio
-bus = SMBus(1)
-sensor = MLX90614(bus, address=0x5a)
+#logging
+logging.basicConfig(format='[%(levelname)s] %(asctime)s: %(message)s')
+LOG = logging.getLogger()
+LOG.setLevel(20)
+
+#initialize temperature temperature sensor bus and gpio
+BUS = SMBus(1)
+TEMP_SENSOR = MLX90614(BUS, address=0x5a)
 
 #LED setup
-greenLed = 24
-redLed = 23
+GREEN_LED = 27
+RED_LED = 17
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(greenLed, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(redLed, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(GREEN_LED, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RED_LED, GPIO.OUT, initial=GPIO.LOW)
 
-#Servo motor setup
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(servoPin, GPIO.OUT)
-# pwm = GPIO.PWM(servoPin, 50)
+#servo motor setup
+SERVO_MOTOR = 18
+PWM_SERVO = pigpio.pi() 
+PWM_SERVO.set_mode(SERVO_MOTOR, pigpio.OUTPUT)
+PWM_SERVO.set_PWM_frequency( SERVO_MOTOR, 50 )
 
-servo = 14
- 
-# more info at http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
- 
-pwm = pigpio.pi() 
-pwm.set_mode(servo, pigpio.OUTPUT)
-pwm.set_PWM_frequency( servo, 50 )
-
-
-
-#pwm.start(2.5)
-
-#Buzzer setup
-buzz = 21
-GPIO.setup(buzz, GPIO.OUT)
-GPIO.output(buzz, GPIO.HIGH)
+#buzzer setup
+BUZZER = 22
+GPIO.setup(BUZZER, GPIO.OUT)
+GPIO.output(BUZZER, GPIO.HIGH)
 
 #IR sensor setup
-ir = 26
-GPIO.setup(ir, GPIO.IN)
-
-
-
-def sendMessage(title, msg):
-    # subprocess.Popen(['notify-send', msg])
-    return
+IR_SENSOR = 5
+GPIO.setup(IR_SENSOR, GPIO.IN)
 
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
@@ -83,7 +69,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	# pass the blob through the network and obtain the face detections
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
-	print(detections.shape)
+	LOG.info(f"Face detected: {detections.shape}")
 
 	# initialize our list of faces, their corresponding locations,
 	# and the list of predictions from our face mask network
@@ -137,70 +123,73 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	# locations
 	return (locs, preds)
 
+
 def upload_to_azure(frame):
     filename = f"UnwellPerson_{datetime.today().strftime('%Y%m%d_%H%M%S')}.jpg"
     temp_file_path = f"temp/{filename}"
     cv2.imwrite(temp_file_path, frame)
-    print(f"Uploading to cloud {filename}...")
+    LOG.info(f"Uploading to cloud {filename}...")
 
     try:
         blob_client = container_client.get_blob_client(filename)
         with open(temp_file_path, "rb") as data:
             blob_client.upload_blob(data)
-        print(f"{filename} uploaded!")
+        LOG.info(f"{filename} uploaded!")
     except Exception as e:
-        print(e)
+        LOG.error("%s: %s", type(e).__name__, e)
     
     os.remove(temp_file_path)
 
+def upload_to_thingspeak(temp, label):
+    field1 = f"&field1={label}"
+    field2 = "&field2={:.1f}".format(temp)
+    with urlopen(BASE_URL + field1 + field2) as response:
+        res_body = response.read()
+    LOG.info(f"Successfully send data to Thingspeak: {res_body}")
+
+
 def openGate():
-    # pwm.ChangeDutyCycle(2.0)
-    #pigpio.set_PWM_dutycycle(2.0)
-    pwm.set_servo_pulsewidth( servo, 500 )
-    print( "Gate open" )
+    PWM_SERVO.set_servo_pulsewidth(SERVO_MOTOR, 500)
+    LOG.info("Gate open")
     sleep(5)
-    
-    
+
+
 def closeGate():
-    # pwm.ChangeDutyCycle(12.0)
-    #pigpio.set_PWM_dutycycle(12.0)
-    pwm.set_servo_pulsewidth( servo, 1500 )
+    PWM_SERVO.set_servo_pulsewidth(SERVO_MOTOR, 1500)
     sleep(3)
-    
+
 
 #Apply Algorithm
 def applyLogic(label, frame):
     temp = getTempData()
     if temp >= 35:
-        GPIO.output(buzz, GPIO.LOW)
-        GPIO.output(redLed, GPIO.HIGH)
+        GPIO.output(BUZZER, GPIO.LOW)
+        GPIO.output(RED_LED, GPIO.HIGH)
         upload_to_azure(frame)
         sleep(3)
     elif (label=="No-Mask"):
-        GPIO.output(redLed, GPIO.HIGH)
-        GPIO.output(greenLed, GPIO.LOW)
-        GPIO.output(buzz, GPIO.HIGH)
-        #gateClose = threading.Thread(target=closeGate)
-        #gateClose.start()
+        GPIO.output(RED_LED, GPIO.HIGH)
+        GPIO.output(GREEN_LED, GPIO.LOW)
+        GPIO.output(BUZZER, GPIO.HIGH)
         closeGate()
     else:
-        GPIO.output(buzz, GPIO.HIGH)
-        GPIO.output(greenLed, GPIO.HIGH)
-        GPIO.output(redLed, GPIO.LOW)
-        #gateOpen = threading.Thread(target=openGate)
-        #gateOpen.start()
+        GPIO.output(BUZZER, GPIO.HIGH)
+        GPIO.output(GREEN_LED, GPIO.HIGH)
+        GPIO.output(RED_LED, GPIO.LOW)
         openGate()
-        
+
 
 def getTempData():
-    temp = sensor.get_obj_temp()
+    temp = TEMP_SENSOR.get_obj_temp()
     return temp
 
+
 def closeEverything():
-    GPIO.output(redLed, GPIO.LOW)
-    GPIO.output(greenLed, GPIO.LOW)
-    GPIO.output(buzz, GPIO.HIGH)
+    GPIO.output(RED_LED, GPIO.LOW)
+    GPIO.output(GREEN_LED, GPIO.LOW)
+    GPIO.output(BUZZER, GPIO.HIGH)
     closeGate()
+
 
 def detect_mask(locs, preds, frame):
     for (box, pred) in zip(locs, preds):
@@ -215,20 +204,15 @@ def detect_mask(locs, preds, frame):
         # include the probability in the label
         label_out = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-        #temperature sensor data
+        #temperature TEMP_SENSOR data
         temp = getTempData()
-        #temp = sensor.get_object_1()
         person_temp = "Temp: {:.1f}".format(temp)
         
-        print(label_out)
-        print(person_temp)
-        
+        LOG.info(label_out)
+        LOG.info(person_temp)
+
         send_label = 1 if label == "Mask" else 0
-        field1 = f"&field1={send_label}"
-        field2 = "&field2={:.1f}".format(temp)
-        with urlopen(baseURL + field1 + field2) as response:
-            res_body = response.read()
-        print(f"Successfully send data to Thingspeak: {res_body}")
+        upload_to_thingspeak(temp, send_label)
         
         # display the label and bounding box rectangle on the output
         # frame
@@ -236,22 +220,11 @@ def detect_mask(locs, preds, frame):
         cv2.putText(frame, person_temp, (endX-10, endY), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 0), 2)
         cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
         
-        # dist = GPIO.input(ir)
-        # print(f"ir = {dist}")
-
-        # if dist == 0:
-        #     print("masuk logic ir")
         applyLogic(label, frame)
-        # else:
-        #     closeEverything()
-        
-        #_thread.start_new_thread(applyLogic, (label,))
-
 
 
 # loop over the frames from the video stream
 def run_video(detect_and_predict_mask):
-    #frame_counter = 0
     while True:
         # grab the frame from the threaded video stream and resize it
         # to have a maximum width of 400 pixels
@@ -259,17 +232,12 @@ def run_video(detect_and_predict_mask):
         frame = vs.read()
         # if frame is not None:
         if frame is not None:
-            # frame = imutils.resize(frame, width=200)
-            #cv2.normalize(frame, frame,0,255, cv2.NORM_MINMAX)
             # detect faces in the frame and determine if they are wearing a
             # face mask or not
-            if GPIO.input(ir):
+            if GPIO.input(IR_SENSOR):
                 closeEverything()
-                #(f"ir 1")
             else:
-                #print(f"ir 0")
                 (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-
 
                 # loop over the detected face locations and their corresponding
                 # locations
@@ -283,19 +251,15 @@ def run_video(detect_and_predict_mask):
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
                     break
-        #frame_counter += 1
 
     # do a bit of cleanup
-
     cv2.destroyAllWindows()
-    # pwm.stop()
-    #turning off servo
-    pwm.set_PWM_dutycycle(servo, 0)
-    pwm.set_PWM_frequency( servo, 0 )
+    #turning off SERVO_MOTOR
+    PWM_SERVO.set_PWM_dutycycle(SERVO_MOTOR, 0)
+    PWM_SERVO.set_PWM_frequency( SERVO_MOTOR, 0)
     GPIO.cleanup()
     vs.stop()
     
-
 
 #main function
 if __name__=="__main__":
@@ -307,19 +271,15 @@ if __name__=="__main__":
     # load the face mask detector model from disk
     maskNet = load_model("model.model")
     
-    #opening gate
-    #gate = threading.Thread(target=openGate)
+    # connect azure
     blob_service_client = BlobServiceClient.from_connection_string(conn_str=AZURE_CONNECTION_STRING)
     try:
         container_client = blob_service_client.get_container_client(container=CONTAINER_NAME)
         container_client.get_container_properties()
     except Exception as e:
-        print(e)
+        LOG.error("%s: %s", type(e).__name__, e)
     # initialize the video stream
-    print("[INFO] starting video stream...")
-    #biar ga erro mungkin? reso=208,160. framerate=16####
+    LOG.info("Starting video stream...")
     vs = VideoStream(usePiCamera=True, framerate=32, resolution=(480,320)).start()
     
     run_video(detect_and_predict_mask)
-
-    
